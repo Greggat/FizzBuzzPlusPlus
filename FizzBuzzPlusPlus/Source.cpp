5d985f793088834e5057ef4cbe28c2c75186c046
Austin Gregg <greggat96@gmail.com>
2015-12-17 22:32:32 -0600

    1.0.4

--- FizzBuzzPlusPlus/Source.cpp
+++ FizzBuzzPlusPlus/Source.cpp
@@ -3,10 +3,9 @@
 /* First Attempt */
 /*
 int main(int argv, int argc[]) {
-	int i;
-	for (int x = 1; x <= 100; x++) {
-		bool fizz = ((x % 3) == 0) ? 1 : 0;
-		bool buzz = ((x % 5) == 0) ? 1 : 0;
+	for (int i = 1; i <= 100; i++) {
+		bool fizz = ((i % 3) == 0) ? 1 : 0;
+		bool buzz = ((i % 5) == 0) ? 1 : 0;
 		if (fizz && buzz)
 			printf("FizzBuzz.\n");
 		else if (fizz && !buzz)
@@ -14,20 +13,20 @@ int main(int argv, int argc[]) {
 		else if (!fizz && buzz)
 			printf("Buzz.\n");
 		else
-			printf("%i.\n", x);
+			printf("%i.\n", i);
 	}
-	std::cin >> i;
+	scanf_s("");
 }
 */
 
 int main(int argv, int argc[]) {
-	for (int x = 1; x <= 100; x++) {
-		if (!(x % 3))
+	for (int i = 1; i <= 100; i++) {
+		if (!(i % 3))
 			printf("Fizz");
-		if (!(x % 5))
+		if (!(i % 5))
 			printf("Buzz");
-		if ((x % 5) && (x % 3))
-			printf("%i", x);
+		if ((i % 5) && (i % 3))
+			printf("%i", i);
 		printf(".\n");
 	}
 	scanf_s("");
